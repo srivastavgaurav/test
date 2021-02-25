a99b5ec07070ebb1b47d598f69090d07e5b2b1fe
srivastavgaurav <srivastavgaurav010192@gmail.com>
2021-02-25 21:01:37 +0530

    code changes in TestBusiness class file

--- codetestwithjunitmockito/src/main/java/com/junitmockito/code/codetestwithjunitmockito/business/TestBusiness.java
+++ codetestwithjunitmockito/src/main/java/com/junitmockito/code/codetestwithjunitmockito/business/TestBusiness.java
@@ -1,7 +1,12 @@
 package com.junitmockito.code.codetestwithjunitmockito.business;
 
+import com.junitmockito.code.codetestwithjunitmockito.model.Item;
+
 public class TestBusiness {
 	
-
+	public Item retrieveHardCodedItem() {
+		System.out.println("Hi");
+		return new Item(1, "Ball", 10, 100);
+	}
 
 }
