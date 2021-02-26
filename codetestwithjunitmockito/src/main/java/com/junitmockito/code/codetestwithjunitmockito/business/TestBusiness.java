e628a34d4517dbc2e444497f8315a57591613e4d
srivastavgaurav <srivastavgaurav010192@gmail.com>
2021-02-26 10:14:50 +0530

    TestBusiness inside method removed

--- codetestwithjunitmockito/src/main/java/com/junitmockito/code/codetestwithjunitmockito/business/TestBusiness.java
+++ codetestwithjunitmockito/src/main/java/com/junitmockito/code/codetestwithjunitmockito/business/TestBusiness.java
@@ -4,9 +4,6 @@ import com.junitmockito.code.codetestwithjunitmockito.model.Item;
 
 public class TestBusiness {
 	
-	public Item retrieveHardCodedItem() {
-		System.out.println("Hi");
-		return new Item(1, "Ball", 10, 100);
-	}
+	
 
 }
